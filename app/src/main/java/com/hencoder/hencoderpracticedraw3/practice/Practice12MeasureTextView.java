c80631113a363fa858d7cbbfd8a20e71ad73be59
yanzy <1187997355@qq.com>
2018-11-17 22:24:27 +0800

    practice

--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice12MeasureTextView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice12MeasureTextView.java
@@ -40,7 +40,11 @@ public class Practice12MeasureTextView extends View {
         // 使用 Paint.measureText 测量出文字宽度，让文字可以相邻绘制
 
         canvas.drawText(text1, 50, 200, paint1);
-        canvas.drawText(text2, 50 + 100, 200, paint2);
-        canvas.drawText(text3, 50 + 200, 200, paint1);
+
+        float space1 = paint1.measureText(text1);
+        float space2 = paint2.measureText(text2);
+
+        canvas.drawText(text2, 50 + space1, 200, paint2);
+        canvas.drawText(text3, 50 + space1 + space2, 200, paint1);
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice13GetTextBoundsView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice13GetTextBoundsView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -19,6 +20,7 @@ public class Practice13GetTextBoundsView extends View {
     String text6 = "â";
     int top = 200;
     int bottom = 400;
+    private float[] mSpaces;
 
     public Practice13GetTextBoundsView(Context context) {
         super(context);
@@ -37,6 +39,27 @@ public class Practice13GetTextBoundsView extends View {
         paint1.setStrokeWidth(20);
         paint1.setColor(Color.parseColor("#E91E63"));
         paint2.setTextSize(160);
+
+        mSpaces = new float[6];
+        Rect result = new Rect();
+
+        paint2.getTextBounds(text1, 0, text1.length(), result);
+        mSpaces[0] = - (result.top + result.bottom) / 2;
+
+        paint2.getTextBounds(text2, 0, text2.length(), result);
+        mSpaces[1] = - (result.top + result.bottom) / 2;
+
+        paint2.getTextBounds(text3, 0, text3.length(), result);
+        mSpaces[2] = - (result.top + result.bottom) / 2;
+
+        paint2.getTextBounds(text4, 0, text4.length(), result);
+        mSpaces[3] = - (result.top + result.bottom) / 2;
+
+        paint2.getTextBounds(text5, 0, text5.length(), result);
+        mSpaces[4] = - (result.top + result.bottom) / 2;
+
+        paint2.getTextBounds(text6, 0, text6.length(), result);
+        mSpaces[5] = - (result.top + result.bottom) / 2;
     }
 
     @Override
@@ -50,11 +73,11 @@ public class Practice13GetTextBoundsView extends View {
         // 这种居中算法的优点是，可以让文字精准地居中，分毫不差
 
         int middle = (top + bottom) / 2;
-        canvas.drawText(text1, 100, middle, paint2);
-        canvas.drawText(text2, 200, middle, paint2);
-        canvas.drawText(text3, 300, middle, paint2);
-        canvas.drawText(text4, 400, middle, paint2);
-        canvas.drawText(text5, 500, middle, paint2);
-        canvas.drawText(text6, 600, middle, paint2);
+        canvas.drawText(text1, 100, middle + mSpaces[0], paint2);
+        canvas.drawText(text2, 200, middle + mSpaces[1], paint2);
+        canvas.drawText(text3, 300, middle + mSpaces[2], paint2);
+        canvas.drawText(text4, 400, middle + mSpaces[3], paint2);
+        canvas.drawText(text5, 500, middle + mSpaces[4], paint2);
+        canvas.drawText(text6, 600, middle + mSpaces[5], paint2);
     }
 }
